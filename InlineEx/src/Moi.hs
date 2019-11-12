87f12871d6303d3f304a499f4debdfd253dc7924
Ted Satcher <ted.satcher@gmail.com>
2019-11-12 16:29:05 -0600

    Working version of Moi Functor.

--- InlineEx/src/Moi.hs
+++ InlineEx/src/Moi.hs
@@ -2,19 +2,19 @@
 
 module Moi where
 
+-- Note the order of the function return value.
+-- There are opposite the order of the type args.
+-- That tripped me up along with the way the problem
+-- was written in the book.
 newtype Moi s a =
-    Moi { runMoi :: (s -> (s, a)) }
+    Moi { runMoi :: (s -> (a, s)) }
 
+-- Fmapping modifies the outcome variable, a, not the state.
 instance Functor (Moi s) where
     fmap :: (a -> b) -> Moi s a -> Moi s b
-    fmap f m = Moi (\s -> (s, f $ snd (runMoi m s)))
+    fmap f m = Moi (\s -> (f $ fst (runMoi m s), s))
+    -- The book had the problem setup as
+    -- fmap f (Moi s) = undefined
 
 fakeState :: Int -> (Int, Int)
-fakeState i = (i+1, i)
-
--- Have:
--- s -> (s, a)
--- a -> b
-
--- Moi s b
--- Moi { runMoi :: (s -> (s, b)) }
+fakeState i = (i, i+1)
